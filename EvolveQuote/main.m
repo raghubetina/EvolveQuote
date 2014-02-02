1b64f6edf8c96d036a52915eec007b373eb96bc2
Raghu Betina <git@rugb.com>
2014-02-02 20:23:53 +0530

    new project

---
+++ EvolveQuote/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EvolveQuote
+//
+//  Created by Raghu Betina on 2/2/14.
+//  Copyright (c) 2014 rugb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
